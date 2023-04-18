c68ad711d0b4338abf068a279276c76a3e6d11bf
Hammad <you@example.com>
2023-04-18 23:30:51 +1000

    SkeltonComponent

---
+++ src/Components/ContactPages/AddContact.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AddContact = () => {
+  return (
+    <div>
+    <div>
+        <button className='btn btn-secondary form-control'>Add Contact</button>
+    </div>
+    </div>
+  )
+}
+
+export default AddContact
